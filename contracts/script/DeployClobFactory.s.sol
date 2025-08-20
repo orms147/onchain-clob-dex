452d1ddcd16655637c8afa9ad2aea9f38d6430f9
Nguyen Ha Bach <146624245+orms147@users.noreply.github.com>
2025-08-20 17:16:15 +0700

    update: deploy contract

--- contracts/script/DeployClobFactory.s.sol
+++ contracts/script/DeployClobFactory.s.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
 import "../src/ClobFactory.sol";
+import "../src/Vault.sol";
 
 contract DeployClobFactory is Script {
     function run() external returns (ClobFactory factory) {
         vm.startBroadcast();
 
-        address vaultAddress = vm.envAddress("VAULT_ADDR"); // vault address 
+        address vaultAddress = vm.envAddress("VAULT_ADDR");
 
         factory = new ClobFactory(vaultAddress);
+        
+        try Vault(vaultAddress).transferOwnership(address(factory)) {
+        } catch {}
 
         vm.stopBroadcast();
     }
--- contracts/script/DeployRouter.s.sol
+++ contracts/script/DeployRouter.s.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
 import "../src/Router.sol";
+import "../src/ClobFactory.sol";
 
 contract DeployRouter is Script {
     function run() external returns (Router router) {
         vm.startBroadcast();
 
-        address factoryAddress =  vm.envAddress("FACTORY_ADDR"); // factory address
+        address factoryAddress = vm.envAddress("FACTORY_ADDR");
 
         router = new Router(factoryAddress);
+
+        try ClobFactory(factoryAddress).setRouter(address(router)) {
+        } catch {}
 
         vm.stopBroadcast();
     }
